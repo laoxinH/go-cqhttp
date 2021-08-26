8669983465ac64e6814e1e6dd1a1efdf3e699c9c
老新 <thixin@qq.com>
2021-08-26 21:59:32 +0800

    JD配置文件说明

--- src/funcMod/JDFuncUtils/JDConfig.js
+++ src/funcMod/JDFuncUtils/JDConfig.js
@@ -1,13 +1,18 @@
 // 京东功能配置文件
 module.exports = {
     QL : {
-        username :"laoxin",
+        // 青龙账号名称
+        username :"",
+        // 青龙密码
         password:"laoxin0318",
+        // 青龙的服务器ip
         host:"112.126.71.120",
+        // 青龙的端口
         port:"5700"
     },
     ninja : {
-        url : "http://112.126.71.120:5701/"
+        // ninja的网址
+        url : ""
     },
     // 使用草料二维码api，这里填写模板id（自定义二维码模板）
     qrMould : "5BTEWAi7kp4hMHYqKNBTMa4"
